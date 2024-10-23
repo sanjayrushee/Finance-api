0c26653e821c701ee552b0982b638984e314fd72
Sanjay <152201028+sanjayrushee@users.noreply.github.com>
2024-10-23 09:34:05 +0530

    add listen

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.get('/summary', async (request, response) => {
     }
 });
 
-
+// listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
